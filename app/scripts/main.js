3d8b59681ea4e9d22bfa324ebab387c6096f98df
wanzghen <wangzhen@blueocn.com.cn>
2015-06-09 18:33:06 +0800

    pos size

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,6 +19,42 @@ $(document).ready(function(){
 		})
 	}
 
+	function size(){
+		var poster = {
+			widthFloor: 200,
+			widthCeiling: 300,
+			heightFloor: 200,
+			heightCeiling: 300
+		}
+		var docWidth = $(document).width();
+		var docHeight = $(document).height();
+		var columnMax = Math.floor(docWidth/poster.widthFloor);
+		var columnMin = Math.floor(docWidth/poster.widthCeiling);
+		var averageColumn =
+
+
+		console.log(columnMax,columnMin);
+
+	}
+
+	function position(){
+		//配置，poster的单个上限或下限
+		
+
+		$(document).width();
+		$(document).height();
+
+		$('.poster').each(function (){
+
+		});
+	}
+
+	size();
+
+	$(window).resize(function(){
+		size();
+	});
+
 	$('.poster').click(function(){
 		$(this).addClass("scale-large");
 		console.log($(this).attr("data-target"));
