6d58ae0b0451e67a6359d397d4547afeec6c518f
wanzghen <wangzhen@blueocn.com.cn>
2015-06-10 18:31:55 +0800

    transform-origin&position

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,7 +8,6 @@ $(document).ready(function(){
 		console.log(data);
 		$('.content-container').append(data);
 		$('.content-container').addClass("active");
-
 	}
 
 	function getPage(src){
@@ -20,43 +19,67 @@ $(document).ready(function(){
 	}
 
 	function size(){
+		//配置，poster的单个上限或下限
 		var poster = {
 			widthFloor: 200,
-			widthCeiling: 300,
+			widthCeiling: 800,
 			heightFloor: 200,
-			heightCeiling: 300
+			heightCeiling: 800
 		}
-		var docWidth = $(document).width();
-		var docHeight = $(document).height();
-		var columnMax = Math.floor(docWidth/poster.widthFloor);
-		var columnMin = Math.floor(docWidth/poster.widthCeiling);
-		var averageColumn =
-
-
-		console.log(columnMax,columnMin);
+		var docWidth = $(window).width();
+		var docHeight = $(window).height();
+		var columnMax = docWidth/poster.widthFloor;
+		var columnMin = docWidth/poster.widthCeiling;
+		var averageColumn = Math.floor((columnMax + columnMin)/2);
+		var posterWidth = docWidth/averageColumn;
+		$('.poster').each(function(){
+			$(this).css({'width':posterWidth+"px",'height': posterWidth+"px"});
+		})
+		$('.posters').css({'width': docWidth,'height': docHeight});
+		console.log(columnMax,columnMin,averageColumn,posterWidth);
+		return averageColumn;
 
 	}
 
-	function position(){
-		//配置，poster的单个上限或下限
-		
-
-		$(document).width();
-		$(document).height();
-
-		$('.poster').each(function (){
-
-		});
+	function position(col_num){
+		var posters = $('.poster');
+		var left=0;
+		var top=0;
+		for (var i = 0; i < posters.length; i++) {
+			var w = posters.eq(i).width();
+			var h = posters.eq(i).height();
+			left = (i % col_num) * w;
+			top =  Math.floor((i/col_num)) * h;
+			posters.eq(i).css({'top': top+"px",'left':left+"px"});
+		};
 	}
 
-	size();
+	function getMousePos(event){
+		var e =event || window.event;
+		console.log({'x': e.screenX,'y':e.screenY});
+		return{'x': e.screenX,'y':e.screenY};
+	}
+
+	function countTransformOrigin(obj){
+
+		var docWidth = $(window).width();
+		var docHeight = $(window).height();
+		console.log(obj.x*100/docWidth + "% "+ obj.y*100/docHeight + "% 0px");
+		return obj.x*100/docWidth + "% "+ obj.y*100/docHeight + "% 0px";
+	}
+
+	position(size());
+
 
 	$(window).resize(function(){
-		size();
+		position(size());
 	});
 
 	$('.poster').click(function(){
-		$(this).addClass("scale-large");
+
+		$('.posters').css({'transform-origin':countTransformOrigin(getMousePos(event)),'-webkit-transform-origin':countTransformOrigin(getMousePos(event))});
+		$('.posters').addClass("scale-large");
+		$(this).css({'transform':'translateY(-'+$(this).height()+'px)','-webkit-transform':'translateY(-'+$(this).height()+'px)'});
 		console.log($(this).attr("data-target"));
 		getPage($(this).attr("data-target"));
 	});
